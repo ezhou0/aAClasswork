bad1c0459d71e96383bd27ad3da9fb9f8478974d
Edwin <edwinzhou99@gmail.com>
2020-12-04 11:52:13 -0800

    did 15 minutes pairing

--- week3/day5/DSskeleton/lib/00_tree_node.rb
+++ week3/day5/DSskeleton/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+    attr_reader :children
+    attr_accessor :parent, :value
+    def initialize(value = nil)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node)
+        parent = parent_node
+        parent_node.children << children
+    end
 
 end
