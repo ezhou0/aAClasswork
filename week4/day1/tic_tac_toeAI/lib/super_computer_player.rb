90a4cba16784964bc9bd9fc3f7c1203595a47c78
Brynn <brynnreginajohnson@gmail.com>
2020-12-07 14:03:42 -0600

    def move in SuperComputerPlayer Class

--- week4/day1/tic_tac_toeAI/lib/super_computer_player.rb
+++ week4/day1/tic_tac_toeAI/lib/super_computer_player.rb
@@ -2,7 +2,27 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    possible_moves = node.children
+    possible_moves.each do |child|
+      if child.winning_node?(mark)
+        node = child
+        return node.prev_move_pos if node
+      end
+    end
+    possible_moves.each do |child|
+      if !child.losing_node?(mark)
+        node = child
+        return node.prev_move_pos if node
+      end
+    end
+    raise_error
+
+
   end
+
+
+
 end
 
 if __FILE__ == $PROGRAM_NAME
