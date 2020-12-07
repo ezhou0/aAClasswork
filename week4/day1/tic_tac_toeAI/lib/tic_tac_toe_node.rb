a3a96d3993ab8ef6fbb9cbb05e273dfe1c7bd0ba
Brynn <brynnreginajohnson@gmail.com>
2020-12-07 13:19:28 -0600

    initialize tic_tac_toe_node class and def children

--- week4/day1/tic_tac_toeAI/lib/tic_tac_toe_node.rb
+++ week4/day1/tic_tac_toeAI/lib/tic_tac_toe_node.rb
@@ -1,7 +1,27 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+
+  end
+
+  def children
+    children = []
+    (0..2).each do |row_idx|
+      (0..2).each do |col_idx|
+        pos = [row_idx, col_idx]
+        next unless board.empty?(pos)
+        new_board = @board.dup
+        new_board[pos] = self.next_mover_mark
+        next_mover_mark = (self.next_mover_mark == :x ? :o : :x) 
+        children << TicTacToeNode.new(new_board, next_mover_mark, pos)
+      end
+    end
+    children     
   end
 
   def losing_node?(evaluator)
@@ -12,6 +32,6 @@ class TicTacToeNode
 
   # This method generates an array of all moves that can be made after
   # the current move.
-  def children
-  end
+  # def children
+  # end
 end
