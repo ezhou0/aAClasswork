80f5a059b04f0bcaf9d2b91547b25e125beff597
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2020-12-10 13:53:08 -0800

    Finished tests and methods for  Array#twosum and Array#my_transpose
    Edwin was Navigator

--- week4/day4/TDDproject/lib/array.rb
+++ week4/day4/TDDproject/lib/array.rb
@@ -4,4 +4,25 @@ class Array
         self.each {|el| hash[el]+=1}
         hash.keys
     end
+
+    def twosum
+        arr = []
+        (0...self.length).each do |i|
+            (i+1...self.length).each do |i2|
+                arr << [i,i2] if self[i] + self[i2] == 0
+            end
+        end
+        arr
+    end
+
+    def my_transpose
+        dimension = self.length 
+        new_arr = Array.new(dimension) { Array.new(dimension) }
+        (0...dimension).each do |i|
+            (0...dimension).each do |j|
+                new_arr[j][i] = self[i][j]
+            end
+        end
+        new_arr
+    end
 end
--- week4/day4/TDDproject/spec/array_spec.rb
+++ week4/day4/TDDproject/spec/array_spec.rb
@@ -14,6 +14,27 @@ describe Array do
     end
 
     describe "#twosum" do 
-        
-    end 
+        it "should accept an array as arg" do 
+            expect{[-1,0,2,-2,1].twosum}.not_to raise_error
+        end
+        it "should return the indices that add up to zero" do 
+            expect([-1,0,2,-2,1].twosum).to eq([[0,4],[2,3]])
+        end
+    end
+
+    describe "#my_transpose" do 
+        it "transposes a matrix" do 
+            matrix = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]
+            ]
+            expect(matrix.my_transpose).to eq([
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8]
+            ])
+
+        end
+    end
 end
