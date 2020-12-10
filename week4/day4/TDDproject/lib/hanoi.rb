2a638397c814077b757a9a713b096f0c4b1e3b8a
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2020-12-10 15:29:51 -0800

    Added implementation and tests for Array#move

--- week4/day4/TDDproject/lib/hanoi.rb
+++ week4/day4/TDDproject/lib/hanoi.rb
@@ -1,8 +1,22 @@
 class Hanoi
     attr_reader :disks, :towers
-    def initialize(disks=7)
+    def initialize(disks=7, towers = nil)
         @disks = disks
-        @towers = create_towers(disks)
+        towers.nil? ? @towers = create_towers(disks) : @towers = towers
+    end
+
+    # towers are indicies
+    def move(tower1, tower2)     
+        start_pos, end_pos = towers[tower1], towers[tower2]
+        raise "Empty tower!!" if start_pos.empty? 
+        if end_pos.empty?
+            end_pos << start_pos.pop
+        elsif end_pos[-1] > start_pos[-1]
+            end_pos << start_pos.pop
+        else
+            raise "Invalid move!!"
+        end
+        towers
     end
 
     private
@@ -13,5 +27,5 @@ class Hanoi
     end
 end
 
-# hanoi = Hanoi.new
-# p hanoi.towers
+hanoi = Hanoi.new(3, [[3],[2,1],[]])
+p hanoi.towers
--- week4/day4/TDDproject/spec/hanoi_spec.rb
+++ week4/day4/TDDproject/spec/hanoi_spec.rb
@@ -4,7 +4,7 @@ require 'hanoi'
 describe Hanoi do
     subject(:hanoi) {Hanoi.new(8)}
     let(:sort){Proc.new{|arr| arr == arr.sorted.reverse}}
-    describe "initialize" do 
+    describe "#initialize" do 
         #initialize 3 piles of disks 
         #accepts n amount of total disks
         it "should accept a number of disks" do
@@ -29,6 +29,33 @@ describe Hanoi do
         
         #all piles should be sortedreverse
         
-    end 
+    end
 
+    describe "#move" do 
+        #  valid moves
+            # if tower is empty we can put it there
+            # else last disk must be larger than current disk
+                # if last disk is smaller than current disk than raise an error
+            # can't move from an empty tower
+        it "takes in two numbers representing towers" do 
+            expect{hanoi.move(1,2)}.not_to raise_error
+        end
+
+        context "when adding to a tower" do 
+            hanoi = Hanoi.new(3, [[3],[2,1],[]])  #need to overwrite init to accept default tower
+            it "should not allow moves from empty tower" do 
+                expect{hanoi.move(2, 1)}.to raise_error("Empty tower!!")
+            end
+            it "should always add to an empty tower" do 
+                expect(hanoi.move(1,2)).to eq([[3],[2],[1]])
+            end
+            it "should always add to the tower if the last disk is larger" do 
+                expect(hanoi.move(2,0)).to eq([[3,1],[2],[]])
+            end
+            it "should never add if the last disk is smaller" do 
+                expect{hanoi.move(1,0)}.to raise_error("Invalid move!!")
+            end
+        end
+    #     # it "should always add a disk to empty tower"
+    end
 end
