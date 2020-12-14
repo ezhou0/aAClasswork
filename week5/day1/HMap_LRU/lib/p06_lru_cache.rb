9005d6b63f73e95a34ba90fc13e5e17fcda7f622
Edwin <edwinzhou99@gmail.com>
2020-12-14 14:56:58 -0800

    finished LRU cache

--- week5/day1/HMap_LRU/lib/p06_lru_cache.rb
+++ week5/day1/HMap_LRU/lib/p06_lru_cache.rb
@@ -2,6 +2,8 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
+  attr_accessor :map, :store
+  attr_reader :max, :prc
   def initialize(max, prc)
     @map = HashMap.new
     @store = LinkedList.new
@@ -14,6 +16,13 @@ class LRUCache
   end
 
   def get(key)
+    if map[key]
+      node = map[key]
+      update_node!(node)
+      node.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -24,12 +33,22 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    value = self.prc.call(key)
+    new_node = @store.append(key,value)
+    map[key] = new_node
+    eject! if count > self.max
+    value
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    node.remove
+    map[node.key] = @store.append(node.key, node.val)
   end
 
   def eject!
+    removed = @store.first
+    removed.remove #removed node from list
+    map.delete(removed.key) #removed from map
   end
 end
