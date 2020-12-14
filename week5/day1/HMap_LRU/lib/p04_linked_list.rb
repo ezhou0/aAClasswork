93f7baa6a08b22b5e5ad80d62503c73a671bf7e6
Edwin <edwinzhou99@gmail.com>
2020-12-14 14:00:18 -0800

    added linked lists methods

--- week5/day1/HMap_LRU/lib/p04_linked_list.rb
+++ week5/day1/HMap_LRU/lib/p04_linked_list.rb
@@ -16,6 +16,17 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    #  self.tail.prev.next = new_node
+    # new_node.next = self.tail
+    # self.tail.prev = new_node
+    # new_node.prev = self.tail.prev
+
+    self.prev.next = self.next if self.next
+    self.next.prev = self.prev if self.prev
+    self.next = nil
+    self.prev = nil
+    self
+
   end
 end
 
@@ -35,30 +46,61 @@ class LinkedList
   end
 
   def first
+    self.head.next if !empty?
   end
 
   def last
+    self.tail.prev if !empty?
   end
 
   def empty?
+    self.head.next == self.tail
   end
 
   def get(key)
+    each {|node| return node.val if node.key == key}
   end
 
   def include?(key)
   end
 
   def append(key, val)
+    new_node = Node.new(key,val)
+
+    self.tail.prev.next = new_node
+    new_node.next = self.tail
+    new_node.prev = self.tail.prev
+    self.tail.prev = new_node
+    
+
+    new_node
   end
 
   def update(key, val)
+    each do |node|
+      if node.key == key
+        node.val = val
+        return node
+      end
+    end
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        node.remove
+        return node.val
+      end
+    end
+    nil
   end
 
   def each
+      current_node = self.head.next
+      while current_node != self.tail
+        yield current_node
+        current_node = current_node.next
+      end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
