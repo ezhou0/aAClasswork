0c915621cf05301cf462cb5be94fc3ce51d5bb4b
colewendling <colewendling@utexas.edu>
2020-12-15 17:30:46 -0600

    selections inside SQL database

--- week5/day2/SQLproj/lib/03_select_nobel.rb
+++ week5/day2/SQLproj/lib/03_select_nobel.rb
@@ -43,25 +43,31 @@ def literature_1962
     FROM
       nobels
     WHERE
-      yr = 1962;
+      yr = 1962 AND subject = "literature"
   SQL
 end
 
 def einstein_prize
   # Show the year and subject that won 'Albert Einstein' his prize.
   execute(<<-SQL)
-  SELECT
+    SELECT
+      yr, subject
     FROM
+      nobels
     WHERE
+      winner = 'Albert Einstein';
   SQL
 end
 
 def millennial_peace_prizes
   # Give the name of the 'Peace' winners since the year 2000, including 2000.
   execute(<<-SQL)
-  SELECT
+    SELECT
+      winner
     FROM
+      nobels
     WHERE
+      subject = 'Peace' AND yr >= 2000;
   SQL
 end
 
@@ -69,9 +75,12 @@ def eighties_literature
   # Show all details (yr, subject, winner) of the Literature prize winners
   # for 1980 to 1989 inclusive.
   execute(<<-SQL)
-  SELECT
+    SELECT
+      yr, subject, winner
     FROM
+      nobels
     WHERE
+      subject = 'Literature' AND yr BETWEEN 1980 AND 1989;
   SQL
 end
 
@@ -79,17 +88,23 @@ def presidential_prizes
   # Show all details of the presidential winners: ('Theodore Roosevelt',
   # 'Woodrow Wilson', 'Jimmy Carter')
   execute(<<-SQL)
-  SELECT
+    SELECT
+      yr, subject, winner
     FROM
+      nobels
     WHERE
+      winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter');
   SQL
 end
 
 def nobel_johns
   # Show the winners with first name John
   execute(<<-SQL)
-  SELECT
+    SELECT
+      winner
     FROM
+      nobels
     WHERE
+      winner like 'John%';
   SQL
 end
