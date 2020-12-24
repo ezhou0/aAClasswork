c100a6c80771fbec51b7657c4ab8c676c651dea2
Edwin <edwinzhou99@gmail.com>
2020-12-23 19:06:14 -0500

    finish sql proj

--- week5/day2/SQLproj/lib/07_joins_exercises.rb
+++ week5/day2/SQLproj/lib/07_joins_exercises.rb
@@ -92,6 +92,21 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+    SELECT 
+      movies.yr,
+     COUNT(*)
+    FROM 
+      movies
+    JOIN
+      castings ON  castings.movie_id = movies.id
+    JOIN
+      actors ON actors.id = castings.actor_id
+    WHERE
+      actors.name = 'John Travolta'
+    GROUP BY
+      movies.yr
+    HAVING 
+      COUNT(*) >= 2;
   SQL
 end
 
@@ -99,6 +114,28 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+    SELECT
+      movies.title,
+      actors.name
+    FROM 
+      movies
+    JOIN
+      castings on castings.movie_id = movies.id
+    JOIN
+      actors on actors.id = castings.actor_id
+    WHERE
+      movies.id IN (
+        SELECT
+          movies.id
+        FROM
+          movies
+        JOIN
+          castings on castings.movie_id = movies.id
+        JOIN
+          actors on actors.id = castings.actor_id
+        WHERE 
+          actors.name = 'Julie Andrews'
+      ) AND castings.ord = 1
   SQL
 end
 
@@ -106,6 +143,21 @@ def prolific_actors
   # Obtain a list in alphabetical order of actors who've had at least 15
   # starring roles.
   execute(<<-SQL)
+    SELECT
+      actors.name
+    FROM  
+      actors
+    JOIN
+      castings ON castings.actor_id = actors.id
+    WHERE
+      castings.ord = 1
+    GROUP BY
+      actors.name
+    HAVING
+      COUNT(*) >= 15
+    ORDER BY
+      actors.name
+
   SQL
 end
 
@@ -113,11 +165,51 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT
+      movies.title, COUNT(DISTINCT castings.actor_id) AS actor_count
+    FROM
+      movies
+    JOIN 
+      castings ON castings.movie_id = movies.id
+    WHERE
+      movies.yr = 1978
+    GROUP BY
+      movies.id
+    ORDER BY 
+      COUNT(*) DESC, movies.title ASC;
   SQL
 end
 
 def colleagues_of_garfunkel
   # List all the people who have played alongside 'Art Garfunkel'.
   execute(<<-SQL)
+    SELECT
+      a1.name
+    FROM
+      (
+        SELECT
+          movies.*
+        FROM
+          movies
+        JOIN
+          castings ON castings.movie_id = movies.id
+        JOIN
+          actors ON actors.id = castings.actor_id
+        WHERE
+          actors.name = 'Art Garfunkel'
+      ) AS m1
+    JOIN 
+    (
+      SELECT
+        actors.*,
+        castings.movie_id
+      FROM
+        actors
+      JOIN
+        castings ON castings.actor_id = actors.id
+      WHERE
+        actors.name != 'Art Garfunkel'
+    
+    ) AS a1 ON m1.id = a1.movie_id
   SQL
 end
