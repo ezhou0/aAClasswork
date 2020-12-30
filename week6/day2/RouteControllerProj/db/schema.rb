ae094e4bc05060465d5c7f92d62a3861a50c12b1
Edwin <edwinzhou99@gmail.com>
2020-12-29 19:27:12 -0500

    update user db and create artwork db

--- week6/day2/RouteControllerProj/db/schema.rb
+++ week6/day2/RouteControllerProj/db/schema.rb
@@ -10,16 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_223455) do
+ActiveRecord::Schema.define(version: 2020_12_30_001858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "img_url", null: false
+    t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_artworks_on_artist_id"
+    t.index ["img_url"], name: "index_artworks_on_img_url", unique: true
+    t.index ["title", "artist_id"], name: "index_artworks_on_title_and_artist_id"
+  end
+
+  create_table "changes", force: :cascade do |t|
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
