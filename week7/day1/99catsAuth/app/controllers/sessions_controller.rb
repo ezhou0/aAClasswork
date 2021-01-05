bfbf711e6d9b876edfd9e6611aceef478350bf12
Edwin <edwinzhou99@gmail.com>
2021-01-04 19:58:00 -0500

    fix relog bug in erb, fix redirect bug in session controller

--- week7/day1/99catsAuth/app/controllers/sessions_controller.rb
+++ week7/day1/99catsAuth/app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
         @user = User.find_by_credentials(params[:user][:username],params[:user][:password])
 
         if @user 
-            login_user!(@user)
-            redirect_to cat_url
+            login_user!
+            redirect_to cats_url
         else
             render :new
         end
