434b1fb7cee260e99eb6e914558b31994d2fcb0d
Edwin <edwinzhou99@gmail.com>
2021-01-05 20:34:15 -0500

    add logout and require log in

--- week7/day2/music_app/app/controllers/users_controller.rb
+++ week7/day2/music_app/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+before_action :require_logged_in, except: [:new, :create]
     def create
         @user = User.new(user_params)
         if @user.save!
