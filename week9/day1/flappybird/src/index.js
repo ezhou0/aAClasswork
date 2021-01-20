ad37cf259c2ac21e55d0c96f98ba44aa08105780
droid4alex <droid4alex@gmail.com>
2021-01-19 16:29:25 -0800

    Added flappybird

--- week9/day1/flappybird/src/index.js
+++ week9/day1/flappybird/src/index.js
@@ -1,4 +1,14 @@
 import FlappyBird from './game';
+// import Game from './game'; 
 
 const canvas = document.getElementById('bird-game');
-new FlappyBird(canvas);
+new FlappyBird(canvas).restart();
+
+
+
+// Finally, it's time to see some color on that canvas! You need to do a few final things to make this happen:
+// import Game into index.js
+// find the canvas using getElementById
+// create a new instance of Game using the canvas you found
+// call restart to trigger the first render
+// do all this after the DOM has loaded
